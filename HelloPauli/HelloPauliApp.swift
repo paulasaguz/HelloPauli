41a7a16a09700695c7ce06c7c9bd90fea8e5aa63
paula-kogni <paula@kogni.studio>
2023-11-11 20:52:33 -0600

    Initial Commit

---
+++ HelloPauli/HelloPauliApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloPauliApp.swift
+//  HelloPauli
+//
+//  Created by Paula Johana on 11/11/23.
+//
+
+import SwiftUI
+
+@main
+struct HelloPauliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
